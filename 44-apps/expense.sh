f73ec29eb995a2aac075c5af9c98dfc463ef11a9
chandrika <chandrikaaws55@gmail.com>
2024-11-16 11:06:10 +0530

    confirmed

--- 44-apps/expense.sh
+++ 44-apps/expense.sh
@@ -1,8 +1,17 @@
+# #!/bin/bash
+# dnf install ansible -y
+# cd /tmp
+# git clone https://github.com/chandrika55-97/ansible/expense-project_ansible.git
+# cd ansible/expense-project_ansible  #ansible is folder in a github chandrika55-97
+# ansible-playbook -i inventory.ini mysql.yml 
+# ansible-playbook -i inventory.ini backend.yml 
+# ansible-playbook -i inventory.ini frontend.yml 
+
 #!/bin/bash
 dnf install ansible -y
 cd /tmp
-git clone https://github.com/chandrika55-97/ansible/expense-project_ansible.git
-cd ansible/expense-project_ansible  #ansible is folder in a github chandrika55-97
-ansible-playbook -i inventory.ini mysql.yml 
-ansible-playbook -i inventory.ini backend.yml 
-ansible-playbook -i inventory.ini frontend.yml 
+git clone https://github.com/daws-81s/expense-ansible.git
+cd expense-ansible
+ansible-playbook -i inventory.ini mysql.yaml
+ansible-playbook -i inventory.ini backend.yaml
+ansible-playbook -i inventory.ini frontend.yaml
